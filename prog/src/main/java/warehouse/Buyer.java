070ea03d68eade95f328b79c119fcab831a15183
samurayrus <46180435+samurayrus@users.noreply.github.com>
2020-07-29 12:19:52 +0300

    not run() -> while(true)

--- prog/src/main/java/warehouse/Buyer.java
+++ prog/src/main/java/warehouse/Buyer.java
@@ -18,27 +18,27 @@ public class Buyer extends Thread {
 
     @Override
     public void run() {
-        quantityPurchase++;  //+1 покупка. Считаются не сколько мы покупок сделали, а сколько раз попытались купить. Пришли на склад - там пусто - все равно +1 покупка
-        if (war.getProduct() == 0) {  //Условие выхода. На складе ничего нет
-            inform();
-            ThreadsStarter.phaser.arrive();
-            return;
-        }
-        int thisPurchase = war.change(productsPerBuy); //берем товар со склада. Раздел на две переменные, чтобы вывести в inform количество взятого товара и остаток, который забрали
-        sumPurchase += thisPurchase;
+        while (true) {
+            quantityPurchase++;  //+1 покупка. Считаются не сколько мы покупок сделали, а сколько раз попытались купить. Пришли на склад - там пусто - все равно +1 покупка
+            if (war.getProduct() == 0) {  //Условие выхода. На складе ничего нет
 
-        //Если получили меньше того, что запрашивали - записываем в остаток.
-        // Второе условие, чтобы не выводило лишие запросы в конце (Когда на складе ничего нет
-        if (thisPurchase != productsPerBuy && thisPurchase != 0) {
-            balance = thisPurchase;
-            System.out.println("Забор остатка " + thisPurchase + " Пришло " + productsPerBuy + " запрос ");
-            inform();
-            ThreadsStarter.phaser.arrive();
-            return;  //забрали остаток, значит дальше можно ничего не делать и выводить результат
-        }
+                ThreadsStarter.phaser.arrive();
+                break;
+            }
+            int thisPurchase = war.change(productsPerBuy); //берем товар со склада. Раздел на две переменные, чтобы вывести в inform количество взятого товара и остаток, который забрали
+            sumPurchase += thisPurchase;
 
-        ThreadsStarter.phaser.arriveAndAwaitAdvance();
-        run();
+            //Если получили меньше того, что запрашивали - записываем в остаток.
+            // Второе условие, чтобы не выводило лишие запросы в конце (Когда на складе ничего нет
+            if (thisPurchase != productsPerBuy && thisPurchase != 0) {
+                balance = thisPurchase;
+                System.out.println("Забор остатка " + thisPurchase + " Пришло " + productsPerBuy + " запрос ");
+                ThreadsStarter.phaser.arrive();
+                break;  //забрали остаток, значит дальше можно ничего не делать и выводить результат
+            }
+            ThreadsStarter.phaser.arriveAndAwaitAdvance();
+        }
+        inform();
     }
 
     public void inform() {
